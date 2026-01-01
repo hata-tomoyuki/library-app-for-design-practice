89999beb634a55caa4e1256b827b03c71dda98ee
hata-tomoyuki <h.tomo0602@gmail.com>
2026-01-01 20:49:30 +0900

    bookController修正

--- src/adapter/controllers/bookController.ts
+++ src/adapter/controllers/bookController.ts
@@ -1,26 +1,30 @@
 import { CreateUseCaseInterface } from "../../application/usecases/book/createUseCaseInterface";
 import { CreateRequestDto } from "../../application/dtos/book/createRequestDto";
+import { CreateResponseDto } from "../../application/dtos/book/createResponseDto";
+
+// Server Actionsから受け取る外部形式の型定義
+export type CreateBookServerActionInput = {
+  title: string;
+  author: string;
+  publishedAt: string | Date;
+};
 
 export class BookController {
   constructor(private readonly createUseCase: CreateUseCaseInterface) {}
 
-  async create(req: Request): Promise<Response> {
-    try {
-      const body = await req.json();
-      const requestDto: CreateRequestDto = {
-        title: body.title,
-        author: body.author,
-        publishedAt: new Date(body.publishedAt),
-      };
-      const book = await this.createUseCase.execute(requestDto);
+  async create(
+    input: CreateBookServerActionInput,
+  ): Promise<CreateResponseDto> {
+    const requestDto: CreateRequestDto = {
+      title: input.title,
+      author: input.author,
+      publishedAt:
+        input.publishedAt instanceof Date
+          ? input.publishedAt
+          : new Date(input.publishedAt),
+    };
 
-      return Response.json(book, { status: 201 });
-    } catch (error) {
-      console.log(error);
-      return Response.json(
-        { error: "書籍の作成に失敗しました" },
-        { status: 500 },
-      );
-    }
+    return await this.createUseCase.execute(requestDto);
   }
+
 }
