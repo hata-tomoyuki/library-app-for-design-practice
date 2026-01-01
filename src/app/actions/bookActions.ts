0ce0b4815b9c07423b0510e7b4fe30993ba22b31
hata-tomoyuki <h.tomo0602@gmail.com>
2026-01-01 20:36:26 +0900

    bookのServerAction実装

--- src/app/actions/bookActions.ts
+++ src/app/actions/bookActions.ts
@@ -1,24 +1,23 @@
-'use server';
+"use server";
 
-import { PrismaBookRepository } from '@/adapter/repositories/prismaBookRepository';
-import { CreateUseCase } from '@/application/usecases/book/createUseCase';
-import { CreateRequestDto } from '@/application/dtos/book/createRequestDto';
-import { CreateResponseDto } from '@/application/dtos/book/createResponseDto';
-import { UuidGenerator } from '@/adapter/utils/uuidGenerator';
-import prisma from '@/lib/prisma';
+import { PrismaBookRepository } from "@/adapter/repositories/prismaBookRepository";
+import { CreateUseCase } from "@/application/usecases/book/createUseCase";
+import { CreateRequestDto } from "@/application/dtos/book/createRequestDto";
+import { CreateResponseDto } from "@/application/dtos/book/createResponseDto";
+import { UuidGenerator } from "@/adapter/utils/uuidGenerator";
+import prisma from "@/lib/prisma";
 
 const bookRepository = new PrismaBookRepository(prisma);
 const uuidGenerator = new UuidGenerator();
 const createUseCase = new CreateUseCase(bookRepository, uuidGenerator);
 
 export async function createBook(
-  requestDto: CreateRequestDto
+  requestDto: CreateRequestDto,
 ): Promise<CreateResponseDto> {
   try {
     return await createUseCase.execute(requestDto);
   } catch (error) {
-    console.error('書籍の作成に失敗しました:', error);
-    throw new Error('書籍の作成に失敗しました');
+    console.error("書籍の作成に失敗しました:", error);
+    throw new Error("書籍の作成に失敗しました");
   }
 }
-
