dbe45adbba2346d46b88f8472392107d8164aff7
hata-tomoyuki <h.tomo0602@gmail.com>
2026-01-01 16:42:40 +0900

    セットアップ

---
+++ tools/templates/infrastructureLayer.ts
@@ -0,0 +1,24 @@
+import { capitalize, lowercaseFirst } from '../utils';
+
+export function generateRouter(entityName: string) {
+  const content = `
+import { Router } from 'express';
+import { ${capitalize(
+    entityName
+  )}Controller } from '../../../adapter/controllers/${lowercaseFirst(
+    entityName
+  )}Controller';
+
+export function ${lowercaseFirst(entityName)}Routes(${lowercaseFirst(
+    entityName
+  )}Controller: ${capitalize(entityName)}Controller): Router {
+  const router = Router();
+
+  // Implement routes
+
+  return router;
+}
+`;
+  return content.trim() + '\n';
+};
+
---
+++ tools/utils.ts
@@ -0,0 +1,23 @@
+import path from "path";
+import fs from "fs";
+
+export function capitalize(s: string): string {
+    return s.charAt(0).toUpperCase() + s.slice(1);
+}
+
+export function lowercaseFirst(s: string): string {
+    return s.charAt(0).toLowerCase() + s.slice(1);
+}
+
+export function writeFile(filePath: string, content: string) {
+    const dir = path.dirname(filePath);
+
+    if(!fs.existsSync(dir)) {
+        fs.mkdirSync(dir, { recursive: true });
+    }
+
+    fs.writeFileSync(filePath, content);
+
+    console.log(`File written: ${filePath}`);
+}
+
