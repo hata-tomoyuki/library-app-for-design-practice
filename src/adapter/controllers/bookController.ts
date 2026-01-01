20e1f889c8d830036b56c775ede9867edf5ceac5
hata-tomoyuki <h.tomo0602@gmail.com>
2026-01-01 19:14:02 +0900

    bookのadapter設定

---
+++ src/adapter/controllers/bookController.ts
@@ -0,0 +1,28 @@
+import { CreateUseCaseInterface } from '../../application/usecases/book/createUseCaseInterface';
+import { CreateRequestDto } from '../../application/dtos/book/createRequestDto'
+
+export class BookController {
+  constructor(
+    private readonly createUseCase: CreateUseCaseInterface
+  ) {}
+
+  async create(req: Request): Promise<Response> {
+    try {
+      const body = await req.json();
+      const requestDto: CreateRequestDto = {
+        title: body.title,
+        author: body.author,
+        publishedAt: new Date(body.publishedAt),
+      }
+      const book = await this.createUseCase.execute(requestDto);
+
+      return Response.json(book, { status: 201 });
+    } catch (error) {
+      console.log(error);
+      return Response.json(
+        { error: '書籍の作成に失敗しました' },
+        { status: 500 }
+      );
+    }
+  }
+}
---
+++ src/adapter/repositories/prismaBookRepository.ts
@@ -0,0 +1,42 @@
+import { PrismaClient } from '../../../app/generated/prisma/client';
+import { Book } from '../../domain/entities/book';
+import { BookRepositoryInterface } from '../../domain/repositories/bookRepositoryInterface';
+
+export class PrismaBookRepository implements BookRepositoryInterface {
+  constructor(private readonly prisma: PrismaClient) {}
+
+  async create(book: Book): Promise<Book> {
+    const createdBook = await this.prisma.book.create({
+      data: {
+        id: book.id,
+        title: book.title,
+        author: book.author,
+        publishedAt: book.publishedAt,
+        isAvailable: false,
+        createdAt: book.createdAt,
+        updatedAt: book.updatedAt,
+      },
+    });
+
+    return new Book(
+      createdBook.id,
+      createdBook.title,
+      createdBook.author,
+      createdBook.publishedAt,
+      createdBook.createdAt,
+      createdBook.updatedAt
+    );
+  }
+
+  // async findById(id: string): Promise<Book | null> {
+  //   const foundBook = await this.prisma.book.findUnique({
+  //     where: { id },
+  //   });
+
+  //   if (!foundBook) return null;
+
+  //   return new Book(
+  //     // Add propaties
+  //   );
+  // }
+}
