fde36e9761bedcb9f47136bed8c21a0597f7d9d9
hata-tomoyuki <h.tomo0602@gmail.com>
2026-01-01 19:25:36 +0900

    bookのinfrastructure設定

---
+++ src/adapter/utils/uuidGenerator.ts
@@ -0,0 +1,9 @@
+import { IdGeneratorInterface } from '@/domain/utils/idGeneratorInterface';
+import { randomUUID } from 'crypto';
+
+export class UuidGenerator implements IdGeneratorInterface {
+  generate(): string {
+    return randomUUID();
+  }
+}
+
