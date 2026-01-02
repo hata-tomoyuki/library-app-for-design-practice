23c4cebb658c07f7732e8def15d9ab1f60411de7
hata-tomoyuki <h.tomo0602@gmail.com>
2026-01-02 16:06:32 +0900

    リポジトリにfindByIdメソッド追加

--- src/server/domain/repositories/bookRepositoryInterface.ts
+++ src/server/domain/repositories/bookRepositoryInterface.ts
@@ -2,6 +2,6 @@ import { Book } from "../entities/book";
 
 export interface BookRepositoryInterface {
   create(book: Book): Promise<Book>;
-  // findById(id: string): Promise<Book | null>;
+  findById(id: string): Promise<Book | null>;
   // Define other repository methods as needed
 }
