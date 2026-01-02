cbeb190cd7e82a24c3a5d4cb252aad5ec40a5fad
hata-tomoyuki <h.tomo0602@gmail.com>
2026-01-02 20:09:17 +0900

    リポジトリにupdateメソッド追加

--- src/server/domain/repositories/bookRepositoryInterface.ts
+++ src/server/domain/repositories/bookRepositoryInterface.ts
@@ -4,4 +4,5 @@ export interface BookRepositoryInterface {
   create(book: Book): Promise<Book>;
   findById(id: string): Promise<Book | null>;
   findAll(): Promise<Book[]>;
+  update(book: Book): Promise<Book>;
 }
