4bb5c7b7b72206112f9d4a5ac294f67614cbe9e6
hata-tomoyuki <h.tomo0602@gmail.com>
2026-01-02 16:17:02 +0900

    book_findAll_domain実装

--- src/server/domain/repositories/bookRepositoryInterface.ts
+++ src/server/domain/repositories/bookRepositoryInterface.ts
@@ -3,5 +3,5 @@ import { Book } from "../entities/book";
 export interface BookRepositoryInterface {
   create(book: Book): Promise<Book>;
   findById(id: string): Promise<Book | null>;
-  // Define other repository methods as needed
+  findAll(): Promise<Book[]>;
 }
